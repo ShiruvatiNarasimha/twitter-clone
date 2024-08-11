e934a25ca6b3897d3eae31f64126b86b3f46ec02
Shiruvati Narasimha <mrnattyboy05@gmail.com>
2024-08-11 19:28:42 +0530

    Update some code

--- src/components/posts/CommentFeed.tsx
+++ src/components/posts/CommentFeed.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import CommentItem from "@/components/posts/CommentItem";
 
 interface commentFeedProps {
-    comments?: Record<string, any>[];
+    comments?: Record<string, any >[];
 }
 const CommentFeed: React.FC<commentFeedProps> = ({ comments = [] }) => {
     return (
         <>
-            {comments.map((comment) => (
-                <CommentItem key={comment.id} data={comment}/>
+            {comments.map ((comment) => (
+                <CommentItem key = {comment.id} data={comment}/>
             ))}
         </>
     );
